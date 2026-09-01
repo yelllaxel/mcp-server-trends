ad248727e94c46cbc9a2d15f778f4f3011788b9e
yelllaxel <yelizaveta.s@boredpanda.com>
2026-09-01 16:14:40 +0300

    user

--- mcp_serv/db_docker/02_create_user.sql
+++ mcp_serv/db_docker/02_create_user.sql
@@ -1,3 +1,5 @@
 CREATE USER 'mcp_analytics'@'%' IDENTIFIED BY '1029384756Liza';
+CREATE USER 'mcp_analytics'@'localhost' IDENTIFIED BY '1029384756Liza';
 GRANT SELECT ON trend_tracking_sample.* TO 'mcp_analytics'@'%';
+GRANT SELECT ON trend_tracking_sample.* TO 'mcp_analytics'@'localhost';
 FLUSH PRIVILEGES;
